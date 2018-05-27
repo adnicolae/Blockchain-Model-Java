5d74cbaa119ba897755096c34e58a9ccc54a0084
Andrei Nicolae <u1516780@live.warwick.ac.uk>
2018-05-27 22:25:38 +0100

    Test mining 5 block as difficulty = 5.

--- src/main/java/com/mycompany/blockychain/App.java
+++ src/main/java/com/mycompany/blockychain/App.java
@@ -5,20 +5,42 @@ import java.util.ArrayList;
 
 /**
  * Entry point of the application.
- *
+ * Test is done using 5 blocks at a difficulty of 6.
+ * As the difficulty increases, the computational power required
+ * to generate the hashes is observed to increase, as expected.
  */
 public class App 
 {
     public static ArrayList<Block> blockchain = new ArrayList<Block>();
-
-    public static void main( String[] args )
+    // Mining difficulty for proof of work.
+    public static int difficulty = 5;
+    
+    public static void main(String[] args)
     {
-        // Create a 3-block chain and output as Json
+        // Create a 5-block chain and output as Json.
         blockchain.add(new Block("This is the genesis block", "0"));
+        System.out.println("Mining block 1 ...");
+        blockchain.get(0).mineBlock(difficulty);
+        
         blockchain.add(new Block("This is the second block", blockchain.get(blockchain.size()-1).hash));
+        System.out.println("Mining block 2 ...");
+        blockchain.get(1).mineBlock(difficulty);
+        
         blockchain.add(new Block("This is the third block", blockchain.get(blockchain.size()-1).hash));
-
+        System.out.println("Mining block 3 ...");
+        blockchain.get(2).mineBlock(difficulty);
+        
+        blockchain.add(new Block("This is the fourth block", blockchain.get(blockchain.size()-1).hash));
+        System.out.println("Mining block 4 ...");
+        blockchain.get(3).mineBlock(difficulty);
+        
+        blockchain.add(new Block("This is the fifth block", blockchain.get(blockchain.size()-1).hash));
+        System.out.println("Mining block 5 ...");
+        blockchain.get(4).mineBlock(difficulty);
+        
+        
         String blockJSON = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+        System.out.println("==== BLOCKCHAIN ====");
         System.out.println(blockJSON);
         System.out.println(isChainValid() ? "Blockchain is valid." : "Blockchain is not valid.");
     }
