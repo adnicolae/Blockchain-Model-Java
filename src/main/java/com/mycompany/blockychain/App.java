64d12beaf2e1e3227c760c8334cf2f2d26a7ade3
Andrei Nicolae <u1516780@live.warwick.ac.uk>
2018-05-27 19:44:47 +0100

    Added method to check the blockchain integrity.

--- src/main/java/com/mycompany/blockychain/App.java
+++ src/main/java/com/mycompany/blockychain/App.java
@@ -20,5 +20,30 @@ public class App
 
         String blockJSON = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
         System.out.println(blockJSON);
+        System.out.println(isChainValid() ? "Blockchain is valid." : "Blockchain is not valid.");
+    }
+    
+    // Check blockchain integrity.
+    public static Boolean isChainValid() {
+    	Block current;
+    	Block previous;
+    	
+    	for (int i = 1; i < blockchain.size(); i++) {
+    		current = blockchain.get(i);
+    		previous = blockchain.get(i-1);
+    		
+    		// Compare the registered hash and computed hash.
+    		if (!current.hash.equals(current.computeHash())) {
+    			System.out.println("Current hashes not equal.");
+    			return false;
+    		}
+    		
+    		// Compare previous hash in the chain with the registered previous hash.
+    		if (!previous.hash.equals(current.previousBlockHash)) {
+    			System.out.println("Previous hashes not equal.");
+    			return false;
+    		}
+    	}
+    	return true;
     }
 }
