b7944d577bd60ba22112a35aa680a182850e0e2c
Andrei Nicolae <u1516780@live.warwick.ac.uk>
2018-05-27 16:52:55 +0100

    Test if block signatures are being generated.

--- src/main/java/com/mycompany/blockychain/App.java
+++ src/main/java/com/mycompany/blockychain/App.java
@@ -1,13 +1,22 @@
 package com.mycompany.blockychain;
+import com.mycompany.blockychain.Models.Block;
 
 /**
- * Hello world!
+ * Entry point of the application.
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        // Round 1 of testing
+        Block genesis = new Block("Hi, I'm the first block!", "0");
+        System.out.println("Hash for Block 1: " + genesis.hash);
+
+        Block second = new Block("Hi, I'm the second block!", genesis.hash);
+        System.out.println("Hash for Block 2: " + second.hash);
+
+        Block third = new Block("Hi, I'm the third block!", second.hash);
+        System.out.println("Hash for Block 3: " + third.hash);
     }
 }
